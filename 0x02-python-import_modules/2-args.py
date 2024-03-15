c73aa688c6f32190b32f182908f5646a733eb5c8
abubeker <abakabrak@gmail.com>
2024-03-14 19:40:26 -0700

    number of args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     if len(argv) == 0:
         print("0 arguments.")
     else:
-        print("{:d} arguments:".format(len(argv) - 1))
+        if len(argv) == 2:
+            print("{:d} argument:".format(1))
+        else:
+            print("{:d} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{:d}: {:s}".format(i, argv[i]))
